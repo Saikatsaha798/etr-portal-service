36c983db0faaf138867df058e2d49ff12bb3f61a
Saikat Saha <saikatsaha798@gmail.com>
2026-01-31 00:38:22 +0530

    Sprint project made

---
+++ src/main/java/com/etr/portal/service/EtrPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.etr.portal.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtrPortalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtrPortalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etr/portal/service/EtrPortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etr.portal.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtrPortalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
