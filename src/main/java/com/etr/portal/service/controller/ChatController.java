d61d4cdbfff7342a298873e25c07a793e75ac02d
Saikat Saha <saikatsaha798@gmail.com>
2026-02-05 23:38:43 +0530

    Imported framework here

--- src/main/java/com/etr/portal/service/controller/ChatController.java
+++ src/main/java/com/etr/portal/service/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.etr.portal.service.controller;
 
+import com.java.model.ChatObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,6 @@ public class ChatController {
 
     @GetMapping("/fetch-all/{userId}")
     public ResponseEntity<?> getAllChatsOfUser (String userId) {
-        return new ResponseEntity<>();
+        return ResponseEntity.ok(new ChatObject());
     }
 }
