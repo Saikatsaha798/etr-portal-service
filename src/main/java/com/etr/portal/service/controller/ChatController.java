4be9d2c9a78af3d9069cafe66cbd6d9bc90bb24d
Saikat Saha <saikatsaha798@gmail.com>
2026-02-07 14:30:56 +0530

    Update ChatController.java

--- src/main/java/com/etr/portal/service/controller/ChatController.java
+++ src/main/java/com/etr/portal/service/controller/ChatController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The type Chat controller.
+ */
 @RestController
 @RequestMapping("chat")
 public class ChatController {
 
+    /**
+     * Gets all chats of user.
+     *
+     * @param userId the user id
+     * @return the all chats of user
+     */
     @GetMapping("/fetch-all/{userId}")
     public ResponseEntity<?> getAllChatsOfUser (String userId) {
         return ResponseEntity.ok(new ChatObject());
